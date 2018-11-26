d4afa421d89159d8c12a33d43cf091f12358602b
steven <onekmsa@gmail.com>
2018-11-26 10:04:18 +0000

    change welcome message #1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example, welcome coe member"
 
 	from := ""
 	if r.URL != nil {
